4b63178c3899bcf4d7457041527a8ebe1148685e
Wang Yaoshen <wangyaoshen@bgi.com>
2022-12-15 19:02:54 +0800

    :art:move flag to flag.go

--- main.go
+++ main.go
@@ -18,54 +18,6 @@ var (
 	etcPath = filepath.Join(exPath, "etc")
 )
 
-var (
-	input = flag.String(
-		"input",
-		"",
-		"input excel",
-	)
-	output = flag.String(
-		"output",
-		"",
-		"output excel, default -input.filter.xlsx",
-	)
-	hitList = flag.String(
-		"list",
-		"",
-		"hit list to be filter",
-	)
-	sheetName = flag.String(
-		"sheet",
-		"All variants data",
-		"sheet name to be filter",
-	)
-	checkCol = flag.String(
-		"col",
-		"疾病中文名",
-		"column name to disease to check filter",
-	)
-	sep = flag.String(
-		"sep",
-		"[n]",
-		"sep to split disease info",
-	)
-	includeDisease = flag.String(
-		"include",
-		filepath.Join(etcPath, "includeDisease.list"),
-		"include disease db file",
-	)
-	excludeDisease = flag.String(
-		"exclude",
-		filepath.Join(etcPath, "excludeDisease.list"),
-		"exclude disease db file",
-	)
-	hitCol = flag.String(
-		"hit",
-		"SampleID",
-		"column name of hit to be filter",
-	)
-)
-
 func main() {
 	version.LogVersion()
 	flag.Parse()
